3e4c4424ebf8a3622d074928e5d0afaf59a6d410
Muhammad Amien Fadhillah <42662895+mfadhil21@users.noreply.github.com>
2018-09-13 08:57:21 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,41 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$batas=5;
+function rekursif($i = 1){
+global $batas;
+  echo "*";
+  $i++;
+ 
+  if($i<=$batas){
+  	rekursif($i);
+  }else{
+  	echo"<br>";
+  	$batas--;
+  if($batas !=0){
+  	rekursif();
+ 	 }
+	}
 }
-rekursif(5);
+ rekursif();
+
+
+echo "<br>";
+
+//=============================
+$batas1=1;
+function rekursif1($t=1){
+global $batas1;
+echo "$t";
+$t++;
+
+if($t<=$batas1){
+	rekursif1($t);
+}else {
+	echo "<br>";
+	$batas1++;
+	if($batas1<=5){
+		rekursif1();
+	}
+	}
+}
+rekursif1();
 ?>
